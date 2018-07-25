4b78293fedfa0f727762a14238c53af9db404c6e
zhangbin <ruodee@126.com>
2018-07-25 16:00:49 +0000

    Seeding topics and users data

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //模型保存到数据库的可
-    protected $fillable = ['name','description']
+    protected $fillable = ['name','description'];
 }
--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+	$sentence = $faker->sentence();
+
+	//随机取一个月内的时间
+	$updated_at = $faker->dateTimeThisMonth();
+	//给dataTimeThisMonth()传递一个参数，参数是生成随机本月时间的最大截止时间，这样保证创建时间小于更新时间。
+	$created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
