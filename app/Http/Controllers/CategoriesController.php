defc39cb2302813b95511678d370904eb56154ac
zhangbin <ruodee@126.com>
2018-07-28 14:13:16 +0000

    Topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,9 +9,9 @@ use App\Models\Category;
 class CategoriesController extends Controller
 {
     //创建show方法
-    public function show(Category $category){
+    public function show(Category $category,Request $request,Topic $topic){
     	//读取分类ID关联的话题，并按每20条分页
-    	$topics = Topic::where('category_id',$category->id)->paginate(20);
+    	$topics = $topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
     	//传递变量话题、分类到模板中去
     	return view('topics.index',compact('topics','category'));
     }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -13,4 +13,29 @@ class Topic extends Model
     public function user(){
     	return $this->belongsTo(User::class);
     }
-}
+
+    public function scopeWithOrder($query,$order){
+    	//不同的排序，使用不同的数据读取逻辑
+    	switch($order){
+    		case 'recent':
+    			$query->recent();
+    			break;
+    		default:
+    			$query->recentReplied();
+    			break;
+    	}
+    	return $query->with('user','category');
+
+    }
+    
+    public function scopeRecentReplied($query){
+    	//当话题有新回复时，我们将编写逻辑来更新话题模型的reply_count属性
+    	//此时会自动触发框架对数据模型updated_at时间戳的更新
+
+    	return $query->orderBy('updated_at','desc');
+    }
+    public function scopeRecent($query){
+    	//按照创建时间排序
+    	return $query->orderBy('created_at','desc');
+    }
+    }
