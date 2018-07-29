edd9f1d50c82a2c223dd83a10326f73751069aff
zhangbin <ruodee@126.com>
2018-07-29 14:34:27 +0000

    User can create topic

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,7 +11,7 @@ class TopicObserver
 {
     public function creating(Topic $topic)
     {
-        //
+        $topic->excerpt = make_excerpt($topic->body);
     }
 
     public function updating(Topic $topic)
