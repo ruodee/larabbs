b64a9f074fc582f75dafb70602cf923429e3546e
zhangbin <ruodee@126.com>
2018-08-11 15:50:48 +0000

    Sidebar active users

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
+use App\Models\User;
 
 class CategoriesController extends Controller
 {
     //创建show方法
-    public function show(Category $category,Request $request,Topic $topic){
+    public function show(Category $category,Request $request,Topic $topic,User $user){
     	//读取分类ID关联的话题，并按每20条分页
     	$topics = $topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
+    	$active_users = $user->getActiveUsers();
     	//传递变量话题、分类到模板中去
-    	return view('topics.index',compact('topics','category'));
+    	return view('topics.index',compact('topics','category','active_users'));
     }
 }
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -6,3 +6,21 @@
 		</a>
 	</div>
 </div>
+@if(count($active_users))
+	<div class="panel panel-default">
+		<div class="panel-body active-users">
+			<div class="text-center">活跃用户</div>
+			<hr>
+			@foreach($active_users as $active_user)
+				<a href="{{ route('users.show',$active_user->id) }}" class="media">
+					<div class="media-left media-middle">
+						<img src="{{ $active_user->avatar }}" width="24px" height="24px" class="img-circle media-object" alt="活跃用户">
+					</div>
+					<div class="media-body">
+						<span class="media-heading">{{ $active_user->name }}</span>
+					</div>
+				</a>
+			@endforeach
+		</div>		
+	</div>
+	@endif
