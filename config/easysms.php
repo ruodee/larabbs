50b0d2cf0f863abb18f4acc7897662f91e843494
vagrant <vagrant@homestead>
2018-09-18 10:01:54 +0000

    add yunpian sms

---
+++ config/easysms.php
@@ -0,0 +1,30 @@
+<?php
+return [
+
+	//HTTP请求的超时时间（秒)
+	'timeout' => 5.0,
+
+	//默认发送配置
+	'default' => [
+		//网关调用策略，默认：顺序调用
+		'strategy' => \Overtrue\EastSms\Strategy\OrderStrategy::class,
+
+		//默认可用的发送网关
+		'gateways' => [
+			'yunpian',
+		],
+
+	],
+
+	//可用的网关配置
+	'gateways' => [
+		'errorlog' => [
+			'file' => 'tem/easy-sms.log',
+		],
+		'yunpian' => [
+			'api_key' => env('YUNPIAN_API_KEY'),
+		],
+
+	],
+
+];
