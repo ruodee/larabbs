d85f1b0ca0cfe143a5e2315abadfbe8df6de6875
zhangbin <ruodee@126.com>
2018-11-11 02:00:42 +0000

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,16 @@ $api = app("Dingo\Api\Routing\Router");
 $api->version('v1',[
 	'namespace' => 'App\Http\Controllers\Api',
 	],function($api){
+		$api->group([
+			'middleware' => 'api.throttle',
+			'limit' => config('api.rate_limits.sign.limit'),
+			'expires' => config('api.rate_limits.sign.expires'),
+		],function($api){
 		//短信验证码
 		$api->post('vertificationCodes','VertificationCodesController@store')
 		->name('api.vertificationCodes.store');
 		//用户注册
 		$api->post('users','UsersController@store')
 		->name('api.users.store');
+						});
 	});
