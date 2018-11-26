c696510beaa8f479d6831bb69353613ef05c93f4
zhangbin <ruodee@126.com>
2018-11-26 14:35:52 +0000

    social authorizations

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'phone','email', 'password','introduction','avatar',
+        'weixin_openid','weixin_unionid'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -31,5 +31,8 @@ $api->version('v1',[
 		//图片验证码
 		$api->post('captchas','CaptchasController@store')
 			->name('api.capchas.store');
+		//第三方登录
+		$api->post('socials/{social_type}/authorizations','AuthorizationsController@socialStore')
+			->name('api.socials.authorizations.store');
 						});
 	});
