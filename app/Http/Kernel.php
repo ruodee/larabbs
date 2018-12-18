0d4eb687c10d51cc960c97723da8c8119e001ccf
zhangbin <ruodee@126.com>
2018-12-18 14:11:32 +0000

    locale

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,9 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        //访问节流，类似于 1分钟只能请求10次 的需求，一般在API中使用
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        //接口语言设置
+        'change-locale' => \App\Http\Middleware\ChangeLocale::class,
     ];
 }
