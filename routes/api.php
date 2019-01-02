959eb0741e4f93783e626d24721698c1a5baad55
vagrant <ruodee@126.com>
2019-01-02 05:22:15 +0000

    weapp users store

--- routes/api.php
+++ routes/api.php
@@ -100,6 +100,9 @@ $api->version('v1',[
 		$api->post('weapp/authorizations','AuthorizationsController@weappStore')
 			->name('api.weapp.authorizations.store');
 						});
+		//小程序注册
+		$api->post('weapp/users','UsersController@weappStore')
+			->name('api.weapp.users.store');
 		//刷新token
 		$api->put('authorizations/current','AuthorizationsController@update')
 			->name('api.authorizations.update');
